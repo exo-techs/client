01bc98d0e517a8ca511c06a97cc9d1fe319ed45b
Amit Bourmad <amitburmad@gmail.com>
2019-05-10 17:59:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import {useState, default as React} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var socket = require('socket.io-client')('http://192.168.3.153:3000');
+
 function App() {
+
+  const [sensor, setSensor] = useState({});
+
+  socket.on('connect', function(){
+    console.log('connected');
+    socket.emit('test', 'WORKS!');
+  });
+
+  socket.on('clientData', (data) => {
+    console.log(data);
+    setSensor(data);
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +31,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <p>temprature: {sensor.temprature}</p>
+          <p>humidity: {sensor.humidity}</p>
         </a>
       </header>
     </div>
