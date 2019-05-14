354941503bbba2af5fbfe870b5faa846ecf78092
Amit Bourmad <amitburmad@gmail.com>
2019-05-14 23:18:19 +0300

    Sends userConnect event + toggle relays

--- src/App.js
+++ src/App.js
@@ -2,39 +2,50 @@ import {useState, default as React} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var socket = require('socket.io-client')('http://192.168.3.153:3000');
+var socket = require('socket.io-client')('http://192.168.3.153:3001');
 
 function App() {
 
   const [sensor, setSensor] = useState({});
+  const [relay1, setRelay1] = useState(0);
+  const [relay2, setRelay2] = useState(0);
 
   socket.on('connect', function(){
     console.log('connected');
-    socket.emit('test', 'WORKS!');
+    socket.emit('userConnect', {
+      userId: Date.now() // TODO: Get better id for user
+    });
   });
 
   socket.on('clientData', (data) => {
     console.log(data);
     setSensor(data);
   });
+
+  // TODO: Duplicate code fix
+  const toggleRelay1 = () => {
+    socket.emit("toggleRelay", {
+      pin: 1,
+      state: relay1
+    });
+    setRelay1(!relay1);
+  }
+
+  const toggleRelay2 = () => {
+    socket.emit("toggleRelay", {
+      pin: 2,
+      state: relay2
+    });
+    setRelay2(!relay2);
+  }
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <p>temprature: {sensor.temprature}</p>
           <p>humidity: {sensor.humidity}</p>
-        </a>
-      </header>
+
+          <button onClick={toggleRelay1}>Toggle relay 1</button>
+          <button onClick={toggleRelay2}>Toggle relay 2</button>
     </div>
   );
 }
